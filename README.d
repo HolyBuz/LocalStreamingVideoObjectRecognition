df7daeb1b857387b96a64a45be51e8919db743c8
HolyBuz <32841770+HolyBuz@users.noreply.github.com>
2020-04-20 02:27:23 +0100

    Create README.d

---
+++ README.d
@@ -0,0 +1,16 @@
+# Streaming Image Recognition
+>  In app streaming image recognition using CoreML. 
+
+![](liveStreamingRecognition.gif)
+
+Experimenting image live recognition using SqueezeNet on a streaming AVPlayer. 
+
+#### 🔨 Technologies: UIKit, AVFoundation, CoreML, Vision, No Third Party libraries.
+####  🚀Platform: 📱iOS
+
+## Requirements
+
+- iOS 12.0+
+- Xcode 11.0+
+
+## Thanks for stopping by!
